e2a4ff3fd071cc62fccd46a63f1136ca8af13803
ol222hf <ol222hf@student.lnu.se>
2022-01-02 23:27:48 +0100

    adding basic sum, mean and stdev implementations

---
+++ math.ts
@@ -0,0 +1,15 @@
+const sum = (numbers: number[]) => {
+  return numbers.reduce((a, b) => a + b, 0);
+};
+
+const mean = (numbers: number[]) => {
+  return sum(numbers) / numbers.length;
+};
+
+const stdev = (numbers: number[]) => {
+  const avg = mean(numbers);
+  const variance =
+    sum(numbers.map((x: number) => (x - avg) ** 2)) / numbers.length - 1;
+
+  return Math.sqrt(variance);
+};
