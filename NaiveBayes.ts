b7a0f5ad311ccff0512ac876ad1067e9a169868d
ol222hf <ol222hf@student.lnu.se>
2022-01-03 23:54:59 +0100

    moved functions to NaiveBayes class

--- NaiveBayes.ts
+++ NaiveBayes.ts
@@ -1,7 +1,127 @@
-const fit = (X: number[][], y: number[]): void => {
-  return;
+import { mean, stdev } from "./utils/math";
+
+type Attribute = {
+  mean: number;
+  stdev: number;
+  count: number;
 };
 
-const predict = (X: number[][]): number[] => {
-  return [];
-};
+class NaiveBayes {
+  private model: Map<number, Attribute[]> = new Map();
+
+  fit = (X: number[][], y: number[]): void => {
+    const separated = this.separateByClass(X, y);
+    const summaries = new Map<number, Attribute[]>();
+
+    for (const [label, rows] of separated.entries()) {
+      if (!summaries.has(label)) {
+        summaries.set(label, this.summarizeDataset(rows));
+      }
+    }
+
+    this.model = summaries;
+  };
+
+  private separateByClass = (
+    X: number[][],
+    y: number[]
+  ): Map<number, number[][]> => {
+    const separated = new Map<number, number[][]>();
+
+    for (let i = 0; i < X.length; i++) {
+      const label = y[i];
+      const row = X[i];
+
+      if (!separated.has(label)) separated.set(label, []);
+      separated.get(label)?.push(row);
+    }
+
+    return separated;
+  };
+
+  private summarizeDataset = (dataset: number[][]): Attribute[] => {
+    const summaries = [];
+
+    for (let i = 0; i < dataset[0].length; i++) {
+      const col = dataset.map((x) => x[i]);
+      summaries.push({ mean: mean(col), stdev: stdev(col), count: col.length });
+    }
+
+    return summaries;
+  };
+
+  predict = (X: number[][]): number[] => {
+    const totalRows = X.length;
+
+    const preds = X.map((row) => {
+      const pred = new Map<number, number>();
+
+      for (const [cv, cs] of this.model.entries()) {
+        const count = this.model.get(cv)![0].count;
+        pred.set(cv, count / totalRows);
+        for (const [i, classSummary] of cs.entries()) {
+          const { mean, stdev, count } = classSummary;
+
+          pred.set(
+            cv,
+            pred.get(cv)! +
+              // Comment out next line to go w/o Math.log / Math.exp
+              //this.calculateProbability(row[i], mean, stdev)
+              Math.log(this.calculateProbability(row[i], mean, stdev))
+          );
+        }
+
+        // Comment out next line to go w/o Math.log / Math.exp
+        pred.set(cv, Math.exp(pred.get(cv)!));
+      }
+
+      return pred;
+    });
+
+    return this.getBest(preds);
+  };
+
+  private calculateProbability = (
+    x: number,
+    mean: number,
+    stdev: number
+  ): number => {
+    const exponent = Math.exp(-((x - mean) ** 2 / (2 * stdev ** 2)));
+    return (1 / (Math.sqrt(2 * Math.PI) * stdev)) * exponent;
+  };
+
+  private getBest = (preds: Map<number, number>[]) => {
+    const best: number[] = [];
+
+    for (const pred of preds) {
+      let bestLabel = -1;
+      let bestPred = -1;
+
+      for (const [cv, p] of pred.entries()) {
+        if (bestLabel === -1 || p > bestPred) {
+          bestLabel = cv;
+          bestPred = p;
+        }
+      }
+
+      best.push(bestLabel);
+    }
+
+    return best;
+  };
+
+  accuracyScore = (preds: number[], y: number[]): number => {
+    let accurate_counter = 0;
+
+    for (let i = 0; i < preds.length; i++) {
+      const pred = preds[i];
+      const label = y[i];
+
+      if (pred === label) accurate_counter++;
+    }
+
+    return accurate_counter / y.length;
+  };
+}
+
+export default NaiveBayes;
--- index.ts
+++ index.ts
@@ -1,124 +1,28 @@
-import { loadCSV, strColToFloat, strColToInt } from "./utils/csv";
-import { mean, stdev } from "./utils/math";
+import { loadCSV } from "./utils/csv";
+import NaiveBayes from "./NaiveBayes";
 
 const main = async () => {
   // const dataset = await loadCSV("./data/banknote_authentication.csv");
   const dataset = await loadCSV("./data/iris.csv");
-  for (let i = 0; i < dataset[0].length - 1; i++) {
-    strColToFloat(dataset, i);
-  }
-  strColToInt(dataset, dataset[0].length - 1);
+  const X: number[][] = [];
+  const y: number[] = [];
 
-  const model = summarizeByClass(dataset);
-
-  const preds = dataset.map((row) => {
-    const pred = calculateClassProbabilites(model, row);
-    return pred;
-  });
-
-  console.log(getAccuracy(preds, dataset));
-};
-
-const getAccuracy = (preds: Map<number, number>[], dataset: number[][]) => {
-  let correct_classification = 0;
-
-  for (let i = 0; i < preds.length; i++) {
-    const [label] = dataset[i].slice(-1);
-
-    let bestLabel = null;
-    let bestPred = -1;
-
-    for (const [cv, p] of preds[i]) {
-      if (bestLabel === null || p > bestPred) {
-        bestLabel = cv;
-        bestPred = p;
-      }
-    }
-
-    if (label === bestLabel) correct_classification++;
+  for (const row of dataset) {
+    X.push(row.slice(0, -1).map((x) => parseFloat(x.trim())));
   }
 
-  return correct_classification / dataset.length;
-};
+  const unique = new Set<string>();
 
-const calculateClassProbabilites = (
-  summaries: Map<number, number[][]>,
-  row: number[]
-) => {
-  const totalRows = Array.from(summaries.values()).reduce((sum, cs) => {
-    const [count] = cs[0].slice(-1);
-    return sum + count;
-  }, 0);
-
-  const probabilities = new Map<number, number>();
-
-  for (const [classValue, classSummaries] of summaries.entries()) {
-    const [count] = summaries.get(classValue)![0].slice(-1);
-    probabilities.set(classValue, count / totalRows);
-    for (const [i, classSummary] of classSummaries.entries()) {
-      const [mean, stdev, count] = classSummary;
-
-      probabilities.set(
-        classValue,
-        probabilities.get(classValue)! +
-          calculateProbability(row[i], mean, stdev)
-        // Math.log(calculateProbability(row[i], mean, stdev))
-      );
-    }
-
-    // probabilities.set(classValue, Math.exp(probabilities.get(classValue)!));
+  for (const row of dataset) {
+    const [label] = row.slice(-1);
+    unique.add(label);
+    y.push([...unique].indexOf(label));
   }
 
-  return probabilities;
-};
-
-const calculateProbability = (
-  x: number,
-  mean: number,
-  stdev: number
-): number => {
-  const exponent = Math.exp(-((x - mean) ** 2 / (2 * stdev ** 2)));
-  return (1 / (Math.sqrt(2 * Math.PI) * stdev)) * exponent;
-};
-
-const separateByClass = (dataset: number[][]): Map<number, number[][]> => {
-  const separated = new Map<number, number[][]>();
-
-  for (const vector of dataset) {
-    const classValue = vector[vector.length - 1];
-
-    if (!separated.has(classValue)) {
-      separated.set(classValue, []);
-    }
-
-    separated.get(classValue)?.push(vector);
-  }
-
-  return separated;
-};
-
-const summarizeByClass = (dataset: number[][]): Map<number, number[][]> => {
-  const separated = separateByClass(dataset);
-  const summaries = new Map<number, number[][]>();
-
-  for (const [classValue, rows] of separated.entries()) {
-    if (!summaries.has(classValue)) {
-      summaries.set(classValue, summarizeDataset(rows));
-    }
-  }
-
-  return summaries;
-};
-
-const summarizeDataset = (dataset: number[][]): number[][] => {
-  const summaries = [];
-
-  for (let i = 0; i < dataset[0].length - 1; i++) {
-    const col = dataset.map((x) => x[i]);
-    summaries.push([mean(col), stdev(col), col.length]);
-  }
-
-  return summaries;
+  const NB = new NaiveBayes();
+  NB.fit(X, y);
+  const preds = NB.predict(X);
+  console.log("Accuracy: " + NB.accuracyScore(preds, y));
 };
 
 main();
